559494e27feb34a70324d1776d6d90ca99c10e1b
GeorgeAlkhoury <Georgekhouri@protonmail.com>
2021-11-28 13:02:10 +0100

    Remove all extra spaces from a string

---
+++ removeExtraSpaces.py
@@ -0,0 +1,14 @@
+def removeSpaces(s: str) -> str:
+    s = list(s)
+    for i in range(len(s)-1):
+        if i == 0 and s[i] == ' ':
+            s[i] = ''
+        elif i != 0 and s[i] == ' ' and (s[i+1] in ['.', '!', '?', '-', ':', '=', ' ']):
+            s[i] = ''
+        else:
+            continue
+    return ''.join(s)
+
+if __name__ == "__main__":
+    string_1 = " Hello .   This is   a Python   program !! "
+    print(removeSpaces(string_1))
