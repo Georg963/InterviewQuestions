2843609622481862328fbc8b8a0e935e94d6d59d
GeorgeAlkhoury <Georgekhouri@protonmail.com>
2021-11-28 11:27:05 +0100

    rotate

---
+++ rotate.py
@@ -0,0 +1,14 @@
+'''
+left-rotate an array by specified positions. 
+'''
+def rotate(x: list, k:int) -> list:
+    new_arr = x[k:]
+    for i in range(k):
+        new_arr.append(x[i])
+    return new_arr
+
+
+if __name__ == "__main__":
+    arr = [1,2,3,4,5,6,7]
+    k = 3
+    print(rotate(arr, k)) #[4, 5, 6, 7, 1, 2, 3]
