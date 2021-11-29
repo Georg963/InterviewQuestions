fdea49165a3695165304080d05a13e846abf2432
GeorgeAlkhoury <Georgekhouri@protonmail.com>
2021-11-29 11:59:12 +0100

    count prime numbers less than n

--- countPrimes.py
+++ countPrimes.py
@@ -1,15 +1,21 @@
 '''
 Given an integer n, return the number of prime numbers that are strictly less than n.
 '''
-
 def count(n: int) -> int:
     count = 0
-    for i in range(1, n):
-        if i%2 != 0 and i < n:
+    for i in range(2, n):
+        Flag = True
+        for j in range(2, i):
+            if i%j == 0:
+                Flag = False
+                break
+        if Flag:
             count += 1
+
     return count
 
 if __name__ == "__main__":
     print(count(0))  #0
     print(count(1))  #0
     print(count(10)) #4
+    print(count(20)) #8
